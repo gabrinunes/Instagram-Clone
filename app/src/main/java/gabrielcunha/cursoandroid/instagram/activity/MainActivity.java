fd7146d1f2767e9a3f33e65455cedcf88a6b3cee
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-30 11:05:33 -0300

    Criando a toolbar

--- app/src/main/java/gabrielcunha/cursoandroid/instagram/activity/MainActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroid/instagram/activity/MainActivity.java
@@ -1,16 +1,62 @@
 package gabrielcunha.cursoandroid.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import gabrielcunha.cursoandroid.instagram.R;
+import gabrielcunha.cursoandroid.instagram.helper.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Configura toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario(){
+        try {
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
