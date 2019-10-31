abe9d3ed59555bf8df3fec1d6e1f7e1c169bbf06
Gabriel Cunha <gn_cunha@hotmail.com>
2019-10-31 14:37:33 -0300

    Criando tela de alteracao de dados

---
+++ app/src/main/java/gabrielcunha/cursoandroid/instagram/activity/EditarPerfilActivity.java
@@ -0,0 +1,27 @@
+package gabrielcunha.cursoandroid.instagram.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+import android.os.Bundle;
+
+import gabrielcunha.cursoandroid.instagram.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        //Configura toolbar
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Editar Perfil");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+    }
+}
