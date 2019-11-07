900b673fd317f1f26f5c0f11600bd9b455445cd4
Gabriel Cunha <gn_cunha@hotmail.com>
2019-11-07 12:04:37 -0300

    Carregando lista de filtros

--- app/src/main/java/gabrielcunha/cursoandroid/instagram/activity/FiltroActivity.java
+++ app/src/main/java/gabrielcunha/cursoandroid/instagram/activity/FiltroActivity.java
@@ -3,6 +3,8 @@ package gabrielcunha.cursoandroid.instagram.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,30 +12,46 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import com.zomato.photofilters.FilterPack;
 import com.zomato.photofilters.imageprocessors.Filter;
+import com.zomato.photofilters.utils.ThumbnailItem;
+import com.zomato.photofilters.utils.ThumbnailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import gabrielcunha.cursoandroid.instagram.R;
+import gabrielcunha.cursoandroid.instagram.adapter.AdapterMiniaturas;
+import gabrielcunha.cursoandroid.instagram.helper.RecyclerItemClickListener;
 
 public class FiltroActivity extends AppCompatActivity {
 
-    static{
+    static {
         System.loadLibrary("NativeImageProcessor");
     }
 
     private ImageView imageFotoEscolhida;
     private Bitmap imagem;
     private Bitmap imagemFiltro;
+    private List<ThumbnailItem> listaFiltros;
+
+    private RecyclerView recyclerFiltros;
+    private AdapterMiniaturas adapterMiniaturas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtro);
 
+        listaFiltros = new ArrayList<>();
+
         //Inicializar componentes
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
+        recyclerFiltros = findViewById(R.id.recyclerFiltros);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Filtros");
@@ -44,29 +62,90 @@ public class FiltroActivity extends AppCompatActivity {
 
         //Recupera a imagem escolhida pelo usuário
         Bundle bundle = getIntent().getExtras();
-        if( bundle != null ){
+        if (bundle != null) {
             byte[] dadosImagem = bundle.getByteArray("fotoEscolhida");
-            imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length );
-            imageFotoEscolhida.setImageBitmap( imagem );
+            imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
+            imageFotoEscolhida.setImageBitmap(imagem);
 
-            imagemFiltro = imagem.copy(imagem.getConfig(),true);
-            Filter filter = FilterPack.getAmazonFilter(getApplicationContext());
-            imageFotoEscolhida.setImageBitmap(filter.processFilter(imagemFiltro));
+            //Configura RecyclerView
+            adapterMiniaturas = new AdapterMiniaturas(listaFiltros, getApplicationContext());
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+            recyclerFiltros.setLayoutManager(layoutManager);
+            recyclerFiltros.setAdapter(adapterMiniaturas);
+            recyclerFiltros.setHasFixedSize(true);
+
+            //Adicionar evento de click no RecyclerView
+            recyclerFiltros.addOnItemTouchListener(
+                    new RecyclerItemClickListener(
+                            getApplicationContext(),
+                            recyclerFiltros,
+                            new RecyclerItemClickListener.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(View view, int position) {
+
+                                    ThumbnailItem item = listaFiltros.get(position);
+                                    imagemFiltro = imagem.copy(imagem.getConfig(), true);
+                                    Filter filtro = item.filter;
+                                    imageFotoEscolhida.setImageBitmap(filtro.processFilter(imagemFiltro));
+
+                                }
+
+                                @Override
+                                public void onLongItemClick(View view, int position) {
+
+                                }
+
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                                }
+                            }
+                    )
+            );
+
+            recuperarFiltros();
+
         }
 
+    }
+
+    private void recuperarFiltros() {
+        //LIMPAR ITENS
+        ThumbnailsManager.clearThumbs();
+        listaFiltros.clear();
+
+        //Configurar filtro normal
+        ThumbnailItem item = new ThumbnailItem();
+        item.image = imagem;
+        item.filterName = "Normal";
+        ThumbnailsManager.addThumb(item);
+
+        //Lista todos os filtros
+        List<Filter> filtros = FilterPack.getFilterPack(getApplicationContext());
+        for (Filter filtro : filtros) {
+            ThumbnailItem itemFiltro = new ThumbnailItem();
+            itemFiltro.image = imagem;
+            itemFiltro.filter = filtro;
+            itemFiltro.filterName = filtro.getName();
+
+            ThumbnailsManager.addThumb(itemFiltro);
+        }
+
+        listaFiltros.addAll(ThumbnailsManager.processThumbs(getApplicationContext()));
+        adapterMiniaturas.notifyDataSetChanged();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_filtro,menu);
+        inflater.inflate(R.menu.menu_filtro, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.ic_salvar_postagem:
                 break;
         }
